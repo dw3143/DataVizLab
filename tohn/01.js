e8b6598058b29218f5c2e461a7e77e2183c94634
Magician <88608061+dw3143@users.noreply.github.com>
2023-10-26 23:39:28 +0800

    结课项目
    干啥啥不行组

---
+++ tohn/01.js
@@ -0,0 +1,17 @@
+var 图片=document.getElementById("z1");
+图片.onclick=function(){
+    window.location="./01.html"
+}
+var 图片=document.getElementById("z2");
+图片.onclick=function(){
+    window.location="./02.html"
+}
+var 图片=document.getElementById("z3");
+图片.onclick=function(){
+    window.location="./03.html"
+}
+var 图片=document.getElementById("z4");
+图片.onclick=function(){
+    window.location="./dw/index.html"
+}
+
